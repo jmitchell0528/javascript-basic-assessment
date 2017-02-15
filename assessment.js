579e14c9695dc33133c1c6e8646775b7b6af9f6b
jmitchell0528 <jlm0528@yahoo.com>
2017-02-15 16:39:23 -0600

    My submitted test!

--- assessment.js
+++ assessment.js
@@ -1,10 +1,13 @@
 //Starting the test!!
 // #1 Create a variable called hello and assign it the string 'goodbye'
-
+var hello = "goodbye";
+console.log(hello);
 
 
 // #2 Use the variable iLove to create a new variable called iLoveCode that is assigned the string "I love code"
-var iLove = 'I love';
+var iLove = 'I love', iLoveCode = "I love code";
+
+
 
 // #3  Make an object called bob and give it the following properties
 // bob has a height of 6ft (string)
@@ -12,7 +15,17 @@ var iLove = 'I love';
 // bob has hair, that has style spikey, and color brown (object)
 // bob is not presidentOfTheUnitedStates (boolean)
 // bob likes apples, bananas, and cherries (array of strings)
-
+var bob = {
+	height: "6ft",
+	age: 24,
+	hair: {
+		style: "spikey",
+		color: "brown"
+	},
+	presidentOfTheUnitedStates: false,
+	likes: ["apples", "bananas", "cherries"]
+}
+console.log(bob);
 
 // #4 Change my shirt color to pink using dot notation
 var myShirt = {
@@ -20,16 +33,28 @@ var myShirt = {
 	color: 'red'
 };
 
+myShirt.color = "pink";
+console.log(myShirt.color);
+
 // Change my shirt type to spandex using square bracket notation
 var myOtherShirt = {
 	type: 'polo',
 	color: 'red'
 };
 
+myOtherShirt["type"] = "spandex";
+console.log(myOtherShirt["type"]);
+
 // #5 Create an object that tracks a count of animals in a zoo.  Call it 'zoo'
 // The key should be the animal name(string) and the value should be how many there are.
 // Our zoo has 8 monkeys, 4 giraffes and 2 elephants
+var zoo = {
+	"monkeys": 8,
+	"giraffes": 4,
+	"elephants": 2
+}
 
+console.log(zoo["monkeys"]);
 
 // #6 Loop through this object and change all keys that start with the letter s to have a value of 's'
 
@@ -41,25 +66,38 @@ var snake = {
 	runs: 'legless'
 };
 
+//for (var s in snake.keys) {}
+	console.log(Object.getOwnPropertyNames(snake));
+
+
 //#7 Create an array of strings that are the 7 primary colors in the rainbow - red, orange, yellow, green, blue, indigo, violet (lower-case). Call your array rainbowColors
+var rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+console.log(rainbowColors);
 
 // #8 Using this array do the following
 var heroes = ['superman', 'batman', 'flash'];
 // add 'wonderwoman' to the end
-
+heroes.push("wonderwoman");
+console.log(heroes);
 // remove 'superman' and store him in a variable called firstHero
-
+var firstHero = heroes.shift("superman");
+console.log(heroes);
 // add 'spongebob' to the start of the array
-
+heroes.unshift("spongebob");
+console.log(heroes);
 // remove 'flash' from the array and store him in a variable called secondHero
+var secondHero = heroes.splice(2, 1);
+console.log(secondHero);
 
+console.log(heroes);
 // leave batman in the array but put a copy of him on a variable called thirdHero
+var thirdHero = heroes[1];
+console.log(thirdHero);
 
 
 
-
-// #9 Write a function called addItem that takes in an array, an item, and adds the item to
-// the array.
+// #9 Write a function called addItem that takes in an array and an item, adds the item to
+// the array, and returns the array with the added item.
 
 
 // #10 Write a function called removeItem that takes in an array of strings, and a string.
